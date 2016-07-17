702d1b0f155ae4db1312ae4176e09c24e468f030
Halvor <halvor@mac-15.local>
2016-07-17 22:43:59 +0200

    Implements minimap (partial implementation)

--- src/front.js
+++ src/front.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet, TouchableHighlight } from 'react-native';
+import Popover from 'react-native-popover';
 
 import ContentList from './content-list';
+import MiniMap from './minimap';
 
 export default class Front extends Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      mapModalVisible: false
+    };
+  }
+
+  toggleModal = () => {
+    this.setState({mapModalVisible:!this.state.mapModalVisible});
+  }
+
+
   render(){
     return(
       <View style={styles.container}>
@@ -12,15 +27,23 @@ export default class Front extends Component{
             <Text style={styles.navBarText}>Back</Text>
           </TouchableHighlight>
 
-          <TouchableHighlight onPress={ () => {}} >
-            <Text style={styles.navBarText}>Header</Text>
-          </TouchableHighlight>
+          <View>
+            <TouchableHighlight ref='button' onPress={this.toggleModal}>
+              <Text style={styles.navBarText}>Press me</Text>
+            </TouchableHighlight>
+          </View>
 
           <TouchableHighlight onPress={ () => {this.props.openDrawer()}} >
             <Text style={styles.navBarText}>Open</Text>
           </TouchableHighlight>
         </View>
         <ContentList />
+        <Popover
+        isVisible={this.state.mapModalVisible}
+        onClose={this.toggleModal}
+        fromRect={{x: 200, y: 50, width: 0, height: 0}}>
+          <MiniMap lat={41.44056} lon={22.72778} />
+        </Popover>
       </View>
     );
   }
